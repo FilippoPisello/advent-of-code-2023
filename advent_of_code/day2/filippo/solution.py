1fee0282ec62a5fa8fd062c8023bdcad88a303dd
Filippo Pisello <filippo.pisello@live.it>
2023-12-05 18:09:38 +0100

    Change setup to account for user-specific input

--- advent_of_code/day2/filippo/solution.py
+++ advent_of_code/day2/filippo/solution.py
@@ -1,2 +1,2 @@
 def main(problem_input: list[str]):
-    ...
+    return 1
--- advent_of_code/read_input.py
+++ advent_of_code/read_input.py
@@ -3,11 +3,11 @@ from pathlib import Path
 ROOT = Path(__file__).parent.resolve()
 
 
-def read_txt_input(day_number: int) -> list[str]:
+def read_txt_input(day_number: int, user: str) -> list[str]:
     """Reads the input file for the given day number.
 
     Each line is an element of the returned list.
     """
-    input_file = ROOT / f"input_files/day{day_number}.txt"
+    input_file = ROOT / f"day{day_number}/{user}/input.txt"
     content = input_file.read_text()
     return content.splitlines()
