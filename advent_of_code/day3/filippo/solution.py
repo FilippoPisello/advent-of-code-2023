06a2cb1c5b0f201c3f54ca48eb618e74893a4817
Filippo Pisello <filippo.pisello@live.it>
2023-12-06 19:39:55 +0100

    Filippo continues day 3

--- advent_of_code/day3/filippo/solution.py
+++ advent_of_code/day3/filippo/solution.py
@@ -1,5 +1,7 @@
 import re
+import string
 from dataclasses import dataclass
+from typing import ClassVar
 
 
 def main_part_one(problem_input: list[str]):
@@ -14,14 +16,44 @@ def main_part_two(problem_input: list[str]):
 class Number:
     value: int
     starting_index: int
-
+    ROW_LENGTH: ClassVar[int] = 140
 
     @property
-    def ending_index(self):
-        return self.starting_index + len(str(self.value)) - 1
+    def number_length(self) -> int:
+        return len(str(self.value))
+
+    @property
+    def ending_index(self) -> int:
+        return self.starting_index + self.number_length - 1
+
+    @property
+    def occupied_indexes(self) -> set[int]:
+        return {x for x in range(self.starting_index, self.ending_index + 1)}
+
+    @property
+    def same_row_indexes(self) -> set[int]:
+        if self.starting_index == 0:
+            return set([self.ending_index + 1])
+        elif self.ending_index == self.ROW_LENGTH:
+            return set([self.starting_index - 1])
+        else:
+            return set([self.starting_index - 1, self.ending_index + 1])
+
+    @property
+    def adjacent_row_indexes(self) -> list[int]:
+        return self.same_row_indexes | self.occupied_indexes
 
 
 def extract_numbers_from_line(line: str) -> int:
-    matches = re.findall(r"\d+", line)
-    return [Number(int(x), 0) for x in matches]
+    expr = re.compile(r"\d+")
+    return [
+        Number(int(match.group()), match.span()[0]) for match in expr.finditer(line)
+    ]
 
+
+def has_symbol(line: str, indexes: str) -> bool:
+    symbols = string.punctuation.replace(".", "")
+    for index in indexes:
+        if line[index] in symbols:
+            return True
+    return False
--- advent_of_code/day3/filippo/tests.py
+++ advent_of_code/day3/filippo/tests.py
@@ -1,4 +1,8 @@
-from advent_of_code.day3.filippo.solution import extract_numbers_from_line
+from advent_of_code.day3.filippo.solution import (
+    Number,
+    extract_numbers_from_line,
+    has_symbol,
+)
 
 
 def test_if_single_number_in_row_then_it_is_extracted():
@@ -21,7 +25,49 @@ def test_if_line_has_no_number_then_nothing_is_returned():
 
 
 def test_number_starting_and_ending_position_in_line_is_stored():
-    line = "617*......"
+    line = ".617*....1111.."
     numbers = extract_numbers_from_line(line)
-    assert numbers[0].starting_index == 0
-    assert numbers[0].ending_index == 2
+    assert numbers[0].starting_index == 1
+    assert numbers[0].ending_index == 3
+    assert numbers[1].starting_index == 9
+    assert numbers[1].ending_index == 12
+
+
+class TestNumberStartOfTheRow:
+    def test_indexes_on_the_same_row_is_only_one_after_ending(self):
+        number = Number(10, 0)
+        assert number.same_row_indexes == {2}
+
+    def test_index_on_adjacent_rows_are_same_and_one_after_ending(self):
+        number = Number(10, 0)
+        assert number.adjacent_row_indexes == {0, 1, 2}
+
+
+class TestNumberIsCenterOfTheRow:
+    def test_indexes_on_the_same_row_are_before_and_after(self):
+        number = Number(10, 1)
+        assert number.same_row_indexes == {0, 3}
+
+    def test_index_on_adjacent_rows_are_same_and_before_and_after(self):
+        number = Number(10, 1)
+        assert number.adjacent_row_indexes == {0, 1, 2, 3}
+
+
+class TestNumberIsEndOfRow:
+    def test_indexes_on_the_same_row_is_only_one_before_start(self):
+        number = Number(10, 139)
+        assert number.same_row_indexes == {138}
+
+    def test_index_on_adjacent_rows_are_same_and_one_before_start(self):
+        number = Number(10, 139)
+        assert number.adjacent_row_indexes == {138, 139, 140}
+
+
+class TestSymbolDetection:
+    def test_if_row_contains_symbol_at_one_of_the_indexes_then_true(self):
+        line = "617*......"
+        assert has_symbol(line, {3})
+
+    def test_if_index_matches_dot_then_false(self):
+        line = "617*......"
+        assert not has_symbol(line, {5})
