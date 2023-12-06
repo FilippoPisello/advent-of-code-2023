8298bb0c5bbb1b09629592ca3e586a420e795914
sacha colson <sacha.colson@teampicnic.com>
2023-12-06 10:18:38 +0100

    Sacha proposed solution

--- advent_of_code/day2/sacha/solution.py
+++ advent_of_code/day2/sacha/solution.py
@@ -1,2 +1,39 @@
+from dataclasses import dataclass
+import re
+
+@dataclass
+class Set:
+    red: int
+    green: int
+    blue: int  
+
+
 def main(problem_input: list[str]):
-    ...
+    bag = Set(12,13,14) #red, green, blue
+    succeed_game = 0
+    sum_cube = 0
+    for game in problem_input:
+        helper = {"red": 0, "green":0, "blue":0}
+        game_id = get_game_id(game.split(":")[0])
+        game_sets = game.split(":")[1].split(';')
+        for set in game_sets:
+            color_set = get_colors(set)
+            helper["red"]=max(helper["red"], color_set.red)
+            helper["green"]=max(helper["green"], color_set.green)
+            helper["blue"]=max(helper["blue"], color_set.blue)
+        if helper["red"] <= bag.red and helper["blue"] <= bag.blue and helper["green"] <= bag.green:
+            succeed_game += game_id
+        sum_cube += helper["red"]*helper["green"]*helper["blue"]
+    return succeed_game, sum_cube
+
+def get_game_id(game):
+    id = int(game.split()[1].rstrip(':'))
+    return id
+
+def get_colors(set):
+    red = sum([int(x) for x in re.findall(r'(\d+)\s+red', set)])
+    green = sum([int(x) for x in re.findall(r'(\d+)\s+green', set)])
+    blue = sum([int(x) for x in re.findall(r'(\d+)\s+blue', set)])
+    return Set(red=red,green=green,blue=blue)
+
+
---
+++ advent_of_code/day2/sacha/tests.py
@@ -0,0 +1,21 @@
+from advent_of_code.day2.sacha.solution import main, get_game_id, get_colors, Set
+
+def test_get_id():
+    game = "Game 3: 2 blue, 3 red; 3 green, 3 blue, 6 red; 4 blue, 6 red; 2 green, 2 blue, 9 red; 2 red, 4 blue"
+    test_id = get_game_id(game.split(":")[0])
+    return test_id == 3
+
+def test_get_color():
+    set = ' 2 blue, 3 red'
+    test = get_colors(set)
+    return test == Set(2,0,3)
+
+def test_good_game():
+    game = ["Game 1: 2 blue, 3 red; 3 green, 3 blue, 6 red; 4 blue, 6 red; 2 green, 2 blue, 9 red; 2 red, 4 blue"]
+    test = main(game)
+    return test == 0
+
+def test_bad_game():
+    game = ["Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"]
+    test = main(game)
+    return test == 3
