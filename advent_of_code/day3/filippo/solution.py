c4cf17e7bb09268eb3b53f4a5d92d36b1f4f9207
Filippo Pisello <filippo.pisello@live.it>
2023-12-06 18:27:22 +0100

    Filippo: test 4

--- advent_of_code/day3/filippo/solution.py
+++ advent_of_code/day3/filippo/solution.py
@@ -1,4 +1,6 @@
 import re
+from dataclasses import dataclass
+
 
 def main_part_one(problem_input: list[str]):
     return
@@ -8,6 +10,13 @@ def main_part_two(problem_input: list[str]):
     return
 
 
+@dataclass
+class Number:
+    value: int
+    starting_index: int
+    ending_index: int
+
+
 def extract_numbers_from_line(line: str) -> int:
-   matches = re.findall(r"\d+",line)
-   return [int(x) for x in matches]
+    matches = re.findall(r"\d+", line)
+    return [Number(int(x), 0, 0) for x in matches]
--- advent_of_code/day3/filippo/tests.py
+++ advent_of_code/day3/filippo/tests.py
@@ -11,3 +11,16 @@ def test_if_two_numbers_in_row_then_both_are_extracted():
     line = "467..114.."
     numbers = extract_numbers_from_line(line)
     assert numbers == [467, 114]
+
+
+def test_if_line_has_no_number_then_nothing_is_returned():
+    line = "...*......"
+    numbers = extract_numbers_from_line(line)
+    assert numbers == []
+
+
+def test_number_starting_and_ending_position_in_line_is_stored():
+    line = "617*......"
+    numbers = extract_numbers_from_line(line)
+    assert numbers[0].starting_index == 0
+    assert numbers[0].ending_index == 2
