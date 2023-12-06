581293b5962a3947c96d87de967d45e10aab4252
David Jeanjean <david.jeanjean@teampicnic.com>
2023-12-06 18:55:55 +0100

    David± test 4 done

--- advent_of_code/day3/filippo/solution.py
+++ advent_of_code/day3/filippo/solution.py
@@ -14,9 +14,14 @@ def main_part_two(problem_input: list[str]):
 class Number:
     value: int
     starting_index: int
-    ending_index: int
+
+
+    @property
+    def ending_index(self):
+        return self.starting_index + len(str(self.value)) - 1
 
 
 def extract_numbers_from_line(line: str) -> int:
     matches = re.findall(r"\d+", line)
-    return [Number(int(x), 0, 0) for x in matches]
+    return [Number(int(x), 0) for x in matches]
+
--- advent_of_code/day3/filippo/tests.py
+++ advent_of_code/day3/filippo/tests.py
@@ -4,13 +4,14 @@ from advent_of_code.day3.filippo.solution import extract_numbers_from_line
 def test_if_single_number_in_row_then_it_is_extracted():
     line = "617*......"
     number = extract_numbers_from_line(line)
-    assert number == [617]
+    assert number[0].value == 617
 
 
 def test_if_two_numbers_in_row_then_both_are_extracted():
     line = "467..114.."
     numbers = extract_numbers_from_line(line)
-    assert numbers == [467, 114]
+    assert numbers[0].value == 467
+    assert numbers[1].value == 114
 
 
 def test_if_line_has_no_number_then_nothing_is_returned():
