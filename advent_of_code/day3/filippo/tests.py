1fc490c202545a73614ac252c81aac26d6f1212c
Filippo Pisello <filippo.pisello@live.it>
2023-12-06 18:10:53 +0100

    Filippo: test 2

--- advent_of_code/day3/filippo/tests.py
+++ advent_of_code/day3/filippo/tests.py
@@ -5,3 +5,9 @@ def test_if_single_number_in_row_then_it_is_extracted():
     line = "617*......"
     number = extract_number_from_line(line)
     assert number == 617
+
+
+def test_if_two_numbers_in_row_then_both_are_extracted():
+    line = "467..114.."
+    numbers = extract_number_from_line(line)
+    assert numbers == [467, 114]
