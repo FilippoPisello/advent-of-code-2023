7378b86049650035f982fbf5291a49a5853cb3f0
Filippo <filippo.pisello@live.it>
2023-12-09 13:15:18 +0100

    Filippo: day 4, part two

--- advent_of_code/day4/filippo/solution.py
+++ advent_of_code/day4/filippo/solution.py
@@ -1,22 +1,37 @@
 def main_part_one(problem_input: list[str]):
     total = 0
-    for line in problem_input:
-        winning, owned = parse_line(line)
-        winning_and_owned = winning & owned
-        if not winning_and_owned:
+    parsed_input = parse_problem_info(problem_input)
+    for number_of_winning_and_owned in parsed_input.values():
+        if not number_of_winning_and_owned:
             continue
-        score = 2 ** (len(winning_and_owned) - 1)
+        score = 2 ** (number_of_winning_and_owned - 1)
         total += score
     return total
 
 
 def main_part_two(problem_input: list[str]):
-    return
+    parsed_input = parse_problem_info(problem_input)
+    copies = {card_number: 1 for card_number in parsed_input}
+    for card_number, number_of_winning_and_owned in parsed_input.items():
+        next_card_number = card_number + 1
+        last_affected_card_number = card_number + number_of_winning_and_owned
+        for copy in range(next_card_number, last_affected_card_number + 1):
+            try:
+                copies[copy] += 1 * copies[card_number]
+            except KeyError:
+                continue
+    return sum(copies.values())
 
 
-def parse_line(line: str) -> tuple[set[int], set[int]]:
-    _, line_without_card = line.split(":")
-    winning, owned = line_without_card.split("|")
-    winning = set(int(n) for n in winning.split())
-    owned = set(int(n) for n in owned.split())
-    return winning, owned
+def parse_problem_info(problem_input: list[str]) -> dict[str, int]:
+    output = {}
+    for card_number, line in enumerate(problem_input, start=1):
+        _, line_without_card = line.split(":")
+
+        winning, owned = line_without_card.split("|")
+        winning = set(int(n) for n in winning.split())
+        owned = set(int(n) for n in owned.split())
+
+        output[card_number] = len(winning & owned)
+
+    return output
--- advent_of_code/day4/filippo/tests.py
+++ advent_of_code/day4/filippo/tests.py
@@ -1,11 +1,14 @@
-from advent_of_code.day4.filippo.solution import main_part_one, parse_line
+from advent_of_code.day4.filippo.solution import (
+    main_part_one,
+    main_part_two,
+    parse_problem_info,
+)
 
 
-def test_winning_and_owned_numbers_are_parsed_from_line():
-    line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-    winning, owned = parse_line(line)
-    assert winning == {41, 48, 83, 86, 17}
-    assert owned == {83, 86, 6, 31, 17, 9, 48, 53}
+def test_number_of_winning_and_owned_numbers_are_parsed_from_string():
+    line = ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"]
+    parsed_info = parse_problem_info(line)
+    assert parsed_info[1] == 4
 
 
 def test_example_part_one():
@@ -18,3 +21,15 @@ def test_example_part_one():
         "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
     ]
     assert main_part_one(problem_input) == 13
+
+
+def test_example_part_two():
+    problem_input = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+    ]
+    assert main_part_two(problem_input) == 30
