2c7e685f2a8c76be82fce1a26beefc5278432598
jimvanoosten <vanoostenjim@gmail.com>
2023-12-12 19:13:43 +0100

    day 5 test

--- advent_of_code/day5/jim/solution.py
+++ advent_of_code/day5/jim/solution.py
@@ -1,2 +1,24 @@
+import re
+
 def main(problem_input: list[str]):
-    return "Feyenoord"
+    
+    number_pattern = r'(\d+)'
+    letter_pattern = r'/^[a-z]+$/'
+    seeds_list = []
+    
+    for line in problem_input:
+        print(line)
+        
+        if re.match("seeds", line):
+            line = line.split(":")
+            seeds = re.findall(number_pattern, line[1])
+            seeds = [int(seed) for seed in seeds]
+            seeds_list.append(seeds)
+            
+        if line == "":
+            continue
+        
+        if re.finditer(letter_pattern, line):
+            print(line)
+    
+    return seeds
